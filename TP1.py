d3ff04afad6338b33c3feb2d277a73e07cb358b4
Watch650 <famicanxi4life@gmail.com>
2024-12-28 10:11:46 +0700

    TP1

--- TP1.py
+++ TP1.py
@@ -11,6 +11,13 @@
 #  2. Return True if path exist else False
 # Submit format: TP1.<Ex1>
 
+# Let G be an undirected graph. Prove that the sum of the degrees of vertices of G is always an even number
+#   + Using the matrix representation, find all the connected component of each graph
+#   + Prove that the number of vertices of odd degree in any graph G is even
+#   + To implement the PathExistence(G, s, v) algo
+#   + To implement an algo tha given (G, s, t) return the squence of vertices that composes the path
+#   + Given a graph and a path, to develop an algo for checking if the path is unique
+
 from collections import deque
 
 class Graph:
@@ -68,6 +75,6 @@ start = int(input("Enter start node: "))
 target = int(input("Enter target node: "))
 
 if graph.path_existence(start, target):
-    print("Path exists!")
+    print("Path exists.")
 else:
     print("No path exists.")
